189bafe9258425dc5ab2ac8b69552ebd02a347af
ianorz <fengwenbu@qq.com>
2021-12-14 02:35:34 +0800

    Site updated: 2021-12-14 02:35:25

--- js/fun_title.js
+++ js/fun_title.js
@@ -2,12 +2,12 @@ var OriginTitle = document.title;
 var titleTime;
 document.addEventListener('visibilitychange', function () {
     if (document.hidden) {
-        $('[rel="icon"]').attr('href', "/img/trhx2.png");
+        $('[rel="icon"]').attr('href', "/medias/elogo.png");
         document.title = '页面已崩溃';
         clearTimeout(titleTime);
     }
     else {
-        $('[rel="icon"]').attr('href', "/img/trhx2.png");
+        $('[rel="icon"]').attr('href', "/medias/logo.png");
         document.title = '嘿嘿~骗你的';
         titleTime = setTimeout(function () {
             document.title = OriginTitle;
